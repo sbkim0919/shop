56ba154b44ebe744e286bdbb1f0344ada306fcab
Subin Kim <sbkim0919@gmail.com>
2023-05-07 03:24:10 +0900

    jpa query

--- src/main/java/com/shop/entity/Item.java
+++ src/main/java/com/shop/entity/Item.java
@@ -21,7 +21,7 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;                        // 상품 코드
     @Column(nullable = false, length = 50)
-    private String itemNn;                  // 상품명
+    private String itemNm;                  // 상품명
     private int price;                      // 가격
     private int stockNumber;                // 재고수량
     private String itemDetail;              // 상품 상세 설명
